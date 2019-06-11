de2da935c465c24567294d485d48b44f4b99c684
n-s405 <32849156+n-s405@users.noreply.github.com>
2019-06-11 19:27:06 +0530

    added stereo depth

---
+++ stereo depth/one.py
@@ -0,0 +1,11 @@
+# Python Version 3.7.2 
+import cv2  # Version 3.4.2 
+
+
+
+
+
+# 1 . Camera calibration: Use a bunch of images to infer the focal length and optical centers of your camera
+# 2 . Undistort images: Get rid of lens distortion in the pictures used for reconstruction
+# 3 . Feature matching: Look for similar features between both pictures and build a depth map
+
